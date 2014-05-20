32dff2d96be95b57051af9c88413e54d2cd7feba
Justin Friebel <friebj99@gmail.com>
2014-05-20 09:13:27 -0500

    first commit

---
+++ remove-more-from-url.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Remove more from URL
+Description: Remove more from URL
+*/
+
+function remove_more_link_scroll( $link ) {
+	$link = preg_replace( '|#more-[0-9]+|', '', $link );
+	return $link;
+}
+add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
+
+?>
